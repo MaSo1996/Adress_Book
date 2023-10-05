8efcb0b29f208f7a0919e8e620a6ec88c33e675b
Marcin Sochacki <6991osam@gmail.com>
2023-10-05 19:16:40 +0200

    Created class KsiazkaAdresowa and Uzytkownik

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class KsiazkaAdresowa
+{
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+
+    vector <Uzytkownik> uzytkownicy;
+
+private:
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
+public:
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+};
